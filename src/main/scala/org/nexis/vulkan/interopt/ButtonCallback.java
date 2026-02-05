2af2fe40983e2f6a7af7946900da8afccb9daacd
pochodnicky <pochodnicky@gratex.com>
2026-02-05 15:16:59 +0100

    add mouse input abstractions and refactor debug callback handling

--- src/main/scala/org/nexis/vulkan/interopt/ButtonCallback.java
+++ src/main/scala/org/nexis/vulkan/interopt/ButtonCallback.java
@@ -1,5 +1,6 @@
 package org.nexis.vulkan.interopt;
 
+@FunctionalInterface
 public interface ButtonCallback {
 
     void invoke(long window, int button, int action, int mods);
--- src/main/scala/org/nexis/vulkan/interopt/ScrollCallback.java
+++ src/main/scala/org/nexis/vulkan/interopt/ScrollCallback.java
@@ -1,5 +1,6 @@
 package org.nexis.vulkan.interopt;
 
+@FunctionalInterface
 public interface ScrollCallback {
 
     void invoke(long window, double xOffset, double yOffset);
